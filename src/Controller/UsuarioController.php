305e253d908ac542830e3d8e097b2dbcd1a014b8
Octavio Bernal Vilana <octabevi@protonmail.com>
2022-08-25 14:12:24 +0200

    Modificaciones
    Se añade debugger, se añaden hojas de estilos y se importa bootstrap.
    
    Se manipula css y fichero base.html.twig.

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -24,40 +24,73 @@ class UsuarioController extends AbstractController
      * como ruta inicial. Al acceder a ella se ejecuta la función
      * definida justo debajo suyo.
      */
-    #[Route('/')]
+    #[Route('/', name: 'app_homepage')]
     public function homepage()
     {
-        $lenguajes = [
-          ['idioma' => 'Python', 'uso' => 'Backend'],
-          ['idioma' => 'C', 'uso' => 'Backend'],
-          ['idioma' => 'Java', 'uso' => 'Backend'],
-          ['idioma' => 'C++', 'uso' => 'Backend'],
-          ['idioma' => 'C#', 'uso' => 'Backend'],
-          ['idioma' => 'Visual Basic', 'uso' => 'Backend'],
-          ['idioma' => 'JavaScript', 'uso' => 'Backend/Frontend'],
-          ['idioma' => 'Asssembly Language',  'uso' => 'Backend'],
-          ['idioma' => 'SQL', 'uso' => 'BBDD'],
+        $versiones = [
+          ['versionSymfony' => '1.0 Symfony', 'versionPHP' => '>=5.0', 'ano' => '2007'],
+          ['versionSymfony' => '1.1 Symfony', 'versionPHP' => '>=5.1', 'ano' => '2008'],
+          ['versionSymfony' => '1.2 Symfony', 'versionPHP' => '>=5.2', 'ano' => '2008'],
+          ['versionSymfony' => '1.3 Symfony', 'versionPHP' => '>=5.2.4', 'ano' => '2009'],
+          ['versionSymfony' => '1.4 Symfony', 'versionPHP' => '>=5.2.4', 'ano' => '2009'],
+          ['versionSymfony' => '2.0 Symfony', 'versionPHP' => '>=5.3.2', 'ano' => '2011'],
+          ['versionSymfony' => '2.1 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2012'],
+          ['versionSymfony' => '2.2 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2013'],
+          ['versionSymfony' => '2.3 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2013'],
+          ['versionSymfony' => '2.4 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2013'],
+          ['versionSymfony' => '2.5 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2014'],
+          ['versionSymfony' => '2.6 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2014'],
+          ['versionSymfony' => '2.7 Symfony', 'versionPHP' => '>=5.3.3', 'ano' => '2014'],
+          ['versionSymfony' => '2.8 Symfony', 'versionPHP' => '>=5.3.9', 'ano' => '2015'],
+          ['versionSymfony' => '3.0 Symfony', 'versionPHP' => '>=5.3.9', 'ano' => '2015'],
+          ['versionSymfony' => '3.1 Symfony', 'versionPHP' => '>=5.5.9', 'ano' => '2016'],
+          ['versionSymfony' => '3.2 Symfony', 'versionPHP' => '>=5.5.9', 'ano' => '2016'],
+          ['versionSymfony' => '3.3 Symfony', 'versionPHP' => '>=5.5.9', 'ano' => '2017'],
+          ['versionSymfony' => '3.4 Symfony', 'versionPHP' => '>=5.5.9', 'ano' => '2017'],
+          ['versionSymfony' => '4.0 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2017'],
+          ['versionSymfony' => '4.1 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2018'],
+          ['versionSymfony' => '4.2 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2018'],
+          ['versionSymfony' => '4.3 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2019'],
+          ['versionSymfony' => '4.4 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2019'],
+          ['versionSymfony' => '5.0 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2019'],
+          ['versionSymfony' => '5.1 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2020'],
+          ['versionSymfony' => '5.2 Symfony', 'versionPHP' => '>=7.1.3', 'ano' => '2020'],
         ];
-
+        /**
+         * Renderizamos la vista que hemos creado, y le pasamos como parámetros
+         * el título, la descripción y el array generado.
+         */
         return $this->render('webpages/homepage.html.twig', [
-            'titulo' => 'Página Principal',
-            'descripcion' => 'Página de pruebas para aprender Symfony.',
-            'lenguajesProgram' => $lenguajes
+            'versionado' => $versiones
         ]);
     }
 
-    #[Route('/browse/{name}')]
-    public function nombre($name = null)
+    #[Route('/lenguajes', name: 'app_lenguajes')]
+    public function verInfoArray()
     {
-        if($name)
-             {
-                 return new Response('Hola '.$name);
-             }
-             else
-             {
-                 return new Response("No has introducido un nombre en la URL");
-             }
+        $lenguajes = [
+            ['idioma' => 'Python', 'uso' => 'Backend'],
+            ['idioma' => 'C', 'uso' => 'Backend'],
+            ['idioma' => 'Java', 'uso' => 'Backend'],
+            ['idioma' => 'C++', 'uso' => 'Backend'],
+            ['idioma' => 'C#', 'uso' => 'Backend'],
+            ['idioma' => 'Visual Basic', 'uso' => 'Backend'],
+            ['idioma' => 'JavaScript', 'uso' => 'Backend/Frontend'],
+            ['idioma' => 'Asssembly Language',  'uso' => 'Backend'],
+            ['idioma' => 'SQL', 'uso' => 'BBDD'],
+        ];
 
+        return $this->render('webpages/programlanguajes.html.twig',[
+            'lenguajesProgram' => $lenguajes
+        ]);
+
+        /**
+         * Mediante la función dd se podrá observar el tipo de datos
+         * que le pasamos, en este caso detecta que es un array,
+         * detecta las posiciones y la clave valor.
+
+        dd($lenguajes);
+         * */
     }
 
 }
