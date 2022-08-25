bee6b9ba4fb881f75f644fd39f6540a32b7a026d
Octavio Bernal Vilana <octabevi@protonmail.com>
2022-08-25 10:51:08 +0200

    Practicando con TWIG.
    - Se aprende uso de interpolación.
    - Se aprende estructura de datos heredada de fichero base.html.twig.

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -12,10 +12,12 @@ namespace App\Controller;
  * Utilizamos los componentes de enrutado de Symfony
  * mediante anotaciones.
  */
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class UsuarioController
+class UsuarioController extends AbstractController
 {
     /**
      * Definimos la ruta inicial de symfony '127.0.0.1/'
@@ -25,7 +27,23 @@ class UsuarioController
     #[Route('/')]
     public function homepage()
     {
-        return new Response('Titulo de la página web');
+        $lenguajes = [
+          ['idioma' => 'Python', 'uso' => 'Backend'],
+          ['idioma' => 'C', 'uso' => 'Backend'],
+          ['idioma' => 'Java', 'uso' => 'Backend'],
+          ['idioma' => 'C++', 'uso' => 'Backend'],
+          ['idioma' => 'C#', 'uso' => 'Backend'],
+          ['idioma' => 'Visual Basic', 'uso' => 'Backend'],
+          ['idioma' => 'JavaScript', 'uso' => 'Backend/Frontend'],
+          ['idioma' => 'Asssembly Language',  'uso' => 'Backend'],
+          ['idioma' => 'SQL', 'uso' => 'BBDD'],
+        ];
+
+        return $this->render('webpages/homepage.html.twig', [
+            'titulo' => 'Página Principal',
+            'descripcion' => 'Página de pruebas para aprender Symfony.',
+            'lenguajesProgram' => $lenguajes
+        ]);
     }
 
     #[Route('/browse/{name}')]
